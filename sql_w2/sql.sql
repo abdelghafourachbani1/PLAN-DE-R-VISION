780e77504f765938beb7b63864d9b78c756028ba
Abdelghafour <achbaniabdelghafour8@gmail.com>
2026-02-04 12:17:26 +0100

    add the day chalenge

--- sql_w2/sql.sql
+++ sql_w2/sql.sql
@@ -30,3 +30,31 @@ FROM Clients c
 JOIN Commandes co ON c.id = co.client_id
 JOIN Lignes_Commandes lc ON co.id = lc.commande_id
 JOIN Produits p ON lc.produit_id = p.id;
+
+
+1. Challenge 1 : (GROUP BY) Afficher le chiffre d affaires total par client.
+
+SELECT 
+    cl.nom AS client_name ,
+    SUM(co.montant) 
+FROM Clients cl
+JOIN Commandes co ON cl.id = co.client_id 
+GROUP BY cl.nom ;
+
+2. Challenge 2 : (ORDER BY) Afficher les produits classés par prix, du plus cher au
+moins cher
+
+SELECT 
+    id AS produit_id,
+    prix AS produit_prix
+FROM Produits order by prix DESC;
+
+
+3. Challenge 3 : (HAVING) Afficher uniquement les clients ayant passé plus de 2
+commandes
+
+SELECT 
+    cl.nom AS client_name ,
+    co.id AS commande_id 
+FROM Clients cl 
+JOIN Commandes co ON cl.id = co.client_id ;
