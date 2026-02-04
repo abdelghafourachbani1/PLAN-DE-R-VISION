8dcc61dce853b4108ed52cb2a8b4870a288bc8c0
Abdelghafour <achbaniabdelghafour8@gmail.com>
2026-02-04 03:24:45 +0100

    add day chelnge

--- sql-w1/day1.sql
+++ sql-w1/day1.sql
@@ -1,18 +1,22 @@
 1. Écrire la requête CREATE TABLE pour une table Produits (id, nom,
 prix, stock).
 
-CREATE TABLE Produits(
-    id SERIAL primary key,
-    nom varchar(100) NOT NULL,
-    prix int NOT NULL,
-    stock int not null
+
+CREATE TABLE Produits (
+    id SERIAL PRIMARY KEY,
+    nom VARCHAR(100) NOT NULL,
+    prix DECIMAL(10,2) NOT NULL,
+    stock INT NOT NULL
 );
 
 2. Challenge 2 : Écrire les requêtes INSERT pour ajouter 3 nouveaux produits dans
 la table.
 
-INSERT INTO Produits(nom,prix,stock) 
-VALUES('p1',100,25),('p2',99,12),('p3',250,3);
+INSERT INTO Produits (nom, prix, stock) VALUES
+('p1', 2.5, 100),
+('p2', 5.0, 50),
+('p3', 30.0, 20);
+
 
 3. Challenge 3 : Écrire une requête UPDATE pour augmenter le prix dun produit
 spécifique de 10%, puis une requête DELETE pour supprimer un produit en
@@ -37,3 +41,26 @@ SELECT AVG(produit_prix) FROM produit;
 plus chère.
 
 SELECT MAX(commande_prix) FROM commande ; 
+
+CREATE TABLE Clients (
+    id SERIAL PRIMARY KEY,
+    nom VARCHAR(100) NOT NULL,
+    email VARCHAR(100)
+);
+
+CREATE TABLE Commandes (
+    id SERIAL PRIMARY KEY,
+    client_id INT NOT NULL,
+    montant DECIMAL(10,2) NOT NULL,
+    FOREIGN KEY (client_id) REFERENCES Clients(id)
+);
+
+CREATE TABLE Lignes_Commandes (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    commande_id INT NOT NULL,
+    produit_id INT NOT NULL,
+    quantite INT NOT NULL,
+    FOREIGN KEY (commande_id) REFERENCES Commandes(id),
+    FOREIGN KEY (produit_id) REFERENCES Produits(id)
+);
+
---
+++ sql_w2/sql.sql
@@ -0,0 +1,43 @@
+1. Challenge 1 : (INNER JOIN) Afficher les commandes avec le nom du client
+correspondant (tables Commandes et Clients).
+
+SELECT 
+    co.id AS commande_id,
+    c.nom AS client_nom,
+    co.date_commande
+FROM Commandes co
+INNER JOIN Clients c
+    ON co.client_id = c.id;
+
+
+2. Challenge 2 : (LEFT JOIN) Afficher tous les clients, et le montant de leur
+commande s ils en ont une (afficher NULL sinon)
+
+SELECT 
+    c.nom AS client_nom,
+    SUM(p.prix * lc.quantite) AS montant_commande
+FROM Clients c
+LEFT JOIN Commandes co
+    ON c.id = co.client_id
+LEFT JOIN Lignes_Commandes lc
+    ON co.id = lc.commande_id
+LEFT JOIN Produits p
+    ON lc.produit_id = p.id
+GROUP BY c.nom;
+
+
+3. Challenge 3 : (JOIN 3 tables) Afficher le détail des commandes : nom du client,
+nom du produit et quantité commandée (tables Clients, Commandes,
+Produits, Lignes_Commandes).
+
+SELECT
+    c.nom AS client_nom,
+    p.nom AS produit_nom,
+    lc.quantite
+FROM Clients c
+JOIN Commandes co
+    ON c.id = co.client_id
+JOIN Lignes_Commandes lc
+    ON co.id = lc.commande_id
+JOIN Produits p
+    ON lc.produit_id = p.id;
