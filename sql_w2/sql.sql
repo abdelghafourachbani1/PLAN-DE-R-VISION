867dc57ffa40b0c44f7d8ad748119b237eea2fb2
Abdelghafour <achbaniabdelghafour8@gmail.com>
2026-02-04 10:30:49 +0100

    add day chelnge

--- sql_w2/sql.sql
+++ sql_w2/sql.sql
@@ -1,28 +1,21 @@
 1. Challenge 1 : (INNER JOIN) Afficher les commandes avec le nom du client
 correspondant (tables Commandes et Clients).
 
-SELECT 
-    co.id AS commande_id,
-    c.nom AS client_nom,
-    co.date_commande
+SELECT  co.id AS commande_id,
+    c.nom AS client_nom
 FROM Commandes co
-INNER JOIN Clients c
-    ON co.client_id = c.id;
+JOIN Clients c ON co.client_id = c.id;
 
 
 2. Challenge 2 : (LEFT JOIN) Afficher tous les clients, et le montant de leur
 commande s ils en ont une (afficher NULL sinon)
 
-SELECT 
-    c.nom AS client_nom,
+SELECT  c.nom AS client_nom,
     SUM(p.prix * lc.quantite) AS montant_commande
 FROM Clients c
-LEFT JOIN Commandes co
-    ON c.id = co.client_id
-LEFT JOIN Lignes_Commandes lc
-    ON co.id = lc.commande_id
-LEFT JOIN Produits p
-    ON lc.produit_id = p.id
+LEFT JOIN Commandes co ON c.id = co.client_id
+LEFT JOIN Lignes_Commandes lc ON co.id = lc.commande_id
+LEFT JOIN Produits p ON lc.produit_id = p.id
 GROUP BY c.nom;
 
 
@@ -30,14 +23,10 @@ GROUP BY c.nom;
 nom du produit et quantité commandée (tables Clients, Commandes,
 Produits, Lignes_Commandes).
 
-SELECT
-    c.nom AS client_nom,
+SELECT c.nom AS client_nom,
     p.nom AS produit_nom,
     lc.quantite
 FROM Clients c
-JOIN Commandes co
-    ON c.id = co.client_id
-JOIN Lignes_Commandes lc
-    ON co.id = lc.commande_id
-JOIN Produits p
-    ON lc.produit_id = p.id;
+JOIN Commandes co ON c.id = co.client_id
+JOIN Lignes_Commandes lc ON co.id = lc.commande_id
+JOIN Produits p ON lc.produit_id = p.id;
